f65d05b9b12fd1273f0db542dbb87d3e317393b6
sw-develop <psw00719@naver.com>
2021-07-20 17:05:45 +0900

    [ADD] User Repository

---
+++ src/main/java/com/study/jwttutorial/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.study.jwttutorial.repository;
+
+import com.study.jwttutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByName(String name);
+}
